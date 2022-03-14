409fa5baa1b8909e3990d8f04566483fb563a2a7
itt828 <iittt828>
2022-03-15 01:08:51 +0900

    :tada: init

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>This is itt828's home page</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name:'Home',
+  components:{
+
+  }
+});
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { processExpression } from '@vue/compiler-core';
+import { resolveComponent } from 'vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/vite-sample' : './',
+  resolve: {
+    alias: {
+      '@': path.resolve('__dirname', '/src')
+    }
+  },
+  build: {
+    outDir: 'docs'
+  },
+  plugins: [vue()]
+})
