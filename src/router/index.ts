6b4b7dd8918d4d8184c24142f4017f2595277770
itt828 <iittt828@gmail.com>
2023-06-09 11:05:55 +0900

    :zap: Split up work page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "@/views/HomeView.vue";
+import Works from "@/views/Works.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/works",
+    name: "Works",
+    component: Works,
   },
 ];
 
---
+++ src/views/Works.vue
@@ -0,0 +1,27 @@
+<template>
+  <Section title="Works">
+    <div :class="$style.container">
+      <WorkCard />
+      <WorkCard />
+      <WorkCard />
+      <WorkCard />
+      <WorkCard />
+      <WorkCard />
+      <WorkCard />
+    </div>
+  </Section>
+</template>
+
+<script setup lang="ts">
+import WorkCard from "../components/UI/WorkCard.vue";
+import Section from "../components/UI/Section.vue";
+</script>
+
+<style module>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+  margin: auto;
+}
+</style>
