72bc65022bf31853a36b5fde3beb9ee6fff11ff1
itt828 <iittt828@gmail.com>
2023-07-11 00:01:41 +0900

    :zap: Fix workcard css

--- src/components/UI/WorkCard.vue
+++ src/components/UI/WorkCard.vue
@@ -1,39 +1,54 @@
 <template>
   <div :class="$style.card">
-    <div :class="$style.image" />
+    <div :class="$style.image">
+      <img src="/src/assets/knoq.png" />
+    </div>
     <div :class="$style.description">
       <div :class="$style.title">hoge</div>
       <div>
         <span :class="$style.text">
-          「ではみなさんは、そういうふうに川だと言われたり、乳の流れたあとだと言われたりしていた、このぼんやりと白いものがほんとうは何かご承知ですか」先生は、黒板につるした大きな黒い星座の
+          ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああaaaaaaaaaaaaaaaaaあああああいいいいいいいいい
         </span>
       </div>
     </div>
   </div>
 </template>
+<script setup lang="ts"></script>
 
 <style module lang="scss">
 .card {
-  // height: 423px;
-  // width: 338px;
   border: 2px $accent solid;
-  border-radius: 16px;
+  border-radius: 12px;
+  height: 100%;
+  width: 100%;
   overflow: hidden;
 }
 .image {
-  width: 100%;
-  height: 50%;
-  background-color: #444;
+  height: 60%;
+  display: flex;
+  justify-content: center;
+  // max-height: 200px;
+  // max-height: min(60%, 200px);
+  & > img {
+    object-fit: contain;
+    max-width: 100%;
+    max-height: 100%;
+  }
 }
 .description {
   padding: 0 12px;
 }
 .title {
   margin-bottom: 12px;
-  font-size: 2rem;
+  font-size: 1.25rem;
 }
 .text {
   overflow-wrap: normal;
   font-size: 1rem;
+}
+.skillContainer {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 4px;
 }
 </style>
