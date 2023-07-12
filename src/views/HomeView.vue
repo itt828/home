f9aa2d8aa342c78446892dac3c993fe95fec843b
itt828 <iittt828@gmail.com>
2023-07-12 11:04:53 +0900

    :construction: Junbi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,21 @@
-<template>WIP かっこいいアニメーションなどが入る</template>
+<template>
+  <div :class="$style.container">
+    <div :class="$style.logo"></div>
+  </div>
+</template>
 
 <script setup lang="ts"></script>
+
+<style module lang="scss">
+.container {
+  margin-top: 72px;
+  display: flex;
+  justify-content: center;
+}
+.logo {
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+  font-size: 2.5rem;
+}
+</style>
