d76b5a729e000d459a767aa141a88c6ecf803ce4
itt828 <iittt828@gmail.com>
2025-12-17 16:41:56 +0900

    upd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 export default defineNuxtConfig({
 	modules: ["@nuxt/eslint", "@unocss/nuxt", "@nuxt/content", "@vueuse/nuxt"],
-	ssr: true,
 	devtools: { enabled: true },
 	css: ["@unocss/reset/tailwind.css"],
 	compatibilityDate: "2025-05-15",
