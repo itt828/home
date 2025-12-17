b7ae28dbc16baf36191e162aadc8589f6d4280d1
itt828 <iittt828@gmail.com>
2025-12-17 15:35:53 +0900

    update nuxt-content config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,20 @@
 export default defineNuxtConfig({
-	modules: ['@nuxt/eslint', '@unocss/nuxt', '@nuxt/content', '@vueuse/nuxt'],
+	modules: ["@nuxt/eslint", "@unocss/nuxt", "@nuxt/content", "@vueuse/nuxt"],
 	ssr: true,
 	devtools: { enabled: true },
-	css: ['@unocss/reset/tailwind.css'],
-	compatibilityDate: '2025-05-15',
+	css: ["@unocss/reset/tailwind.css"],
+	compatibilityDate: "2025-05-15",
 	eslint: {
 		config: {
 			stylistic: {
-				indent: 'tab',
+				indent: "tab",
 			},
 		},
 	},
-})
+	content: {
+		database: {
+			type: "d1",
+			bindingName: "home-content",
+		},
+	},
+});
