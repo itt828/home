a27871128745f189b95618b2918ef4d10eb5e3d3
itt828 <iittt828@gmail.com>
2025-12-21 20:00:19 +0900

    update styles

--- components/Timeline.vue
+++ components/Timeline.vue
@@ -1,20 +1,20 @@
 <template>
-  <ol class="relative border-l border-gray-200 ml-3">
-    <li
-      v-for="(item, index) in items"
-      :key="index"
-      class="mb-10 ml-6"
-      :class="{ 'opacity-60': !isCurrent(item) }"
-    >
+  <ol class="relative border-l border-gray-400 ml-3">
+    <li v-for="(item, index) in items" :key="index" class="mb-10 ml-6">
       <div
         class="absolute w-3 h-3 rounded-full mt-1.5 -left-1.5 border border-white"
-        :class="isCurrent(item) ? 'bg-blue-600' : 'bg-gray-200'"
+        :class="isCurrent(item) ? 'bg-blue-600' : 'bg-gray-400'"
       ></div>
-      <time class="mb-1 text-sm font-normal leading-none text-gray-400">
+      <time class="mb-1 text-sm font-normal leading-none text-gray-600">
         {{ item.period[0] }} - {{ item.period[1] || "現在" }}
       </time>
-      <h3 class="text-lg font-semibold text-gray-900">{{ item.title }}</h3>
-      <p class="mb-4 text-base font-normal text-gray-500 whitespace-pre-line">
+      <h3
+        class="text-lg font-semibold text-gray-900"
+        :class="{ 'opacity-50': !isCurrent(item) }"
+      >
+        {{ item.title }}
+      </h3>
+      <p class="mb-4 text-base font-normal text-gray-600 whitespace-pre-line">
         {{ item.description }}
       </p>
     </li>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,19 @@
 <template>
 	<div
-		class="min-h-screen grid grid-rows-[1fr_auto] justify-items-center gap-4 bg-stone-100 overflow-x-hidden"
+		class="min-h-svh grid grid-rows-[1fr_auto] justify-items-center gap-4 bg-stone-100"
 	>
 		<div class="fixed z-10 mt-2 w-full max-w-4xl px-4">
 			<AppHeader />
 		</div>
 		<div
 			v-if="$slots.top"
-			class="w-full"
+			class="w-full mb--16"
 		>
 			<slot name="top" />
 		</div>
-		<div class="w-full max-w-4xl flex flex-col mt-15 px-4">
-			<main>
-				<slot />
-			</main>
-		</div>
+		<main class="w-full max-w-4xl flex flex-col px-4 mt-16">
+			<slot />
+		</main>
 		<AppFooter class="w-full" />
 	</div>
 </template>
